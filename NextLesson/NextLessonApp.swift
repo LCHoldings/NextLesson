334bc741437d3b32b40bd1f33517141c03c32681
Lazyllama <me@lazyllama.xyz>
2024-09-25 20:09:58 +0200

    Initial Commit

---
+++ NextLesson/NextLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextLessonApp.swift
+//  NextLesson
+//
+//  Created by Simon K on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct NextLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
